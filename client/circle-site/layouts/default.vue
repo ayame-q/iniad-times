e43b5a0cb06ed5f2c0eb33d3005951c0eaa6d81b
Ayame <ayame.space@gmail.com>
2021-04-05 10:02:36 +0900

    Google Formに更新

--- client/circle-site/layouts/default.vue
+++ client/circle-site/layouts/default.vue
@@ -13,7 +13,8 @@
 						<nuxt-link to="works">works</nuxt-link>
 					</li>-->
 					<li>
-						<nuxt-link to="entry">entry</nuxt-link>
+						<!--<nuxt-link to="entry">entry</nuxt-link>-->
+						<a href="https://forms.gle/DnCk411WJ8YCt8ABA">入会する</a>
 					</li>
 					<li class="contact-link">
 						<!--<nuxt-link to="contact"><img src="~/assets/img/mail.svg" alt="mail"></nuxt-link>-->
