c1a043bc48f88cbdcfdc22fc0f7edbae4aa754d5
Ayame <ayame.space@gmail.com>
2021-05-03 11:08:58 +0900

    ProfileのAdminを変更

--- server/circle/admin.py
+++ server/circle/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Profile
 
-# Register your models here.
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("family_name", "given_name", "family_name_ruby", "given_name_ruby", "email", "user", "course", "interested_in", "created_at")
+    list_display_links = ("family_name", "given_name")
+
+
+admin.site.register(Profile, ProfileAdmin)
