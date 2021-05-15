c9aeedbcde28a2ececd7678e1d9922282c2e2a6d
Ayame <ayame.space@gmail.com>
2021-05-16 00:25:41 +0900

    Fix plugin path

--- client/circle-site/nuxt.config.js
+++ client/circle-site/nuxt.config.js
@@ -38,7 +38,7 @@ export default {
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
-		'~/plugins/day.js',
+		'@/plugins/day.js',
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
